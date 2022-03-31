a691055effe632b0402381575ef2187ddac0fce7
Faiz Rashid <faiz.rashid@gmx.us>
2022-03-31 20:21:38 +0800

    static test

--- test/functional_test_login.py
+++ test/functional_test_login.py
@@ -3,9 +3,17 @@ from bs4 import BeautifulSoup
 
 # login test cases
 
+def test_static():
+    session = requests.session()
+    login = session.get("http://127.0.0.1:8080/CRMMVC/").text
+    soup = BeautifulSoup(login, "html.parser")
+    print(soup.title.string)
+    fault_text = soup.title.string
+    assert fault_text == "Hello"
+
 def test_login_1():
     session = requests.session()
-    login = session.post("http://127.0.0.1:8080/login", {"username": "admin", "password": "1234"}).text
+    login = session.post("http://127.0.0.1:8080/CRMMVC_war/login.jsp", {"username": "admin", "password": "1234"}).text
     soup = BeautifulSoup(login, "html.parser")
     print(soup.title.string)
     fault_text = soup.title.string
