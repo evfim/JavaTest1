9be838ec335d5f4bf05c28a9acf8263304998b7e
Lam YongXian <lamyongxian@users.noreply.github.com>
2022-03-31 21:18:24 +0800

    Update functional_test_login.py

--- test/functional_test_login.py
+++ test/functional_test_login.py
@@ -7,36 +7,36 @@ def test_static():
     session = requests.session()
     login = session.get("http://127.0.0.1:8080/CRMMVC/").text
     soup = BeautifulSoup(login, "html.parser")
-    print("=========")
-    print(soup)
-    print("=========")
+    #print("=========")
+    #print(soup)
+    #print("=========")
     fault_text = soup.find_all(text=True)
-    assert fault_text == "Hello"
+    assert fault_text in "Hello"
 
 def test_login_1():
     session = requests.session()
-    login = session.post("http://127.0.0.1:8080/CRMMVC_war/login.jsp", {"username": "admin", "password": "1234"}).text
+    login = session.post("http://127.0.0.1:8080/CRMMVC/login", {"username": "admin", "password": "1234"}).text
     soup = BeautifulSoup(login, "html.parser")
-    print(soup.title.string)
-    fault_text = soup.title.string
-    assert fault_text == "Posts"
+    #print(soup.title.string)
+    fault_text = soup.find_all(text=True)
+    assert fault_text in "Posts"
 
 def test_login_2():
     session = requests.session()
-    login = session.post("http://127.0.0.1/CRMMVC_war/login", {"username": "admin", "password": "1234"}).text
+    login = session.post("http://127.0.0.1/CRMMVC/login", {"username": "admin", "password": "1234"}).text
     soup = BeautifulSoup(login, "html.parser")
-    print(soup.title.string)
-    fault_text = soup.title.string
-    assert fault_text == "Wrong Login!"
+    #print(soup.title.string)
+    fault_text = soup.find_all(text=True)
+    assert fault_text in "Wrong Login!"
 
 
 def test_login_3():
     session = requests.session()
-    login = session.post("http://127.0.0.1:8080/CRMMVC_war/login", {"username": "admin", "password": "1234"}).text
+    login = session.post("http://127.0.0.1:8080/CRMMVC/login", {"username": "admin", "password": "1234"}).text
     soup = BeautifulSoup(login, "html.parser")
-    print(soup.title.string)
-    fault_text = soup.title.string
-    assert fault_text == "Wrong Login!"
+    #print(soup.title.string)
+    fault_text = soup.find_all(text=True)
+    assert fault_text in "Wrong Login!"
 
 if __name__ == '__main__':
     test_static()
