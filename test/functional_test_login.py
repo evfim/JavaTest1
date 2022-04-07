3c35be2bfeb32771cda83aa0e35441aa3c2d8a35
Faiz Rashid <faiz.rashid@gmx.us>
2022-04-07 20:34:55 +0800

    debug functional test

--- test/functional_test_login.py
+++ test/functional_test_login.py
@@ -15,8 +15,8 @@ def test_static():
     print(fault_text)
     str_match = [s for s in fault_text if s.__contains__("Hello")]
     print (str_match)
-    
-    assert "Hello" in str_match
+    assert str_match == "Hello NUS!"
+    #assert "Hello" in str_match
 
 def test_login_1():
     session = requests.session()
