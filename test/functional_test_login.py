a2253585f87105b225ac8d0d9cbd11cf4fd1012c
Faiz Rashid <faiz.rashid@gmx.us>
2022-04-07 20:11:28 +0800

    debug functional test

--- test/functional_test_login.py
+++ test/functional_test_login.py
@@ -13,7 +13,9 @@ def test_static():
     fault_text = soup.find_all(text=True)
     print("Fault text value:")
     print(fault_text)
-    assert "Hello" in fault_text
+    fault_text = fault_text.find("Hello")
+    assert fault_text == "Hello"
+    #assert "Hello" in fault_text
 
 def test_login_1():
     session = requests.session()
