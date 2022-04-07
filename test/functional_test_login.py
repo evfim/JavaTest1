67b64789f1ed12eb8bd9c873289ea37153e98fae
Faiz Rashid <faiz.rashid@gmx.us>
2022-04-07 21:09:35 +0800

    login and landing functional test

--- test/functional_test_login.py
+++ test/functional_test_login.py
@@ -2,31 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 # login test cases
-
-def test_static():
-    session = requests.session()
-    login = session.get("http://127.0.0.1:8080/CRMMVC/").text
-    soup = BeautifulSoup(login, "html.parser")
-    #print("=========")
-    #print(soup)
-    #print("=========")
-    fault_text = soup.find_all(text=True)
-    print("Fault text value:")
-    print(fault_text)
-    str_match = [s for s in fault_text if s.__contains__("Hello")]
-    
-    str_match=' '.join(map(str,str_match))
-    print (str_match)
-    #assert str_match == "Hello NUS!"
-    assert "Hello" in str_match
-
 def test_login_1():
     session = requests.session()
     login = session.post("http://127.0.0.1:8080/CRMMVC/login", {"username": "admin", "password": "1234"}).text
     soup = BeautifulSoup(login, "html.parser")
     #print(soup.title.string)
     fault_text = soup.find_all(text=True)
-    assert "Posts" in fault_text
+    str_match = [s for s in fault_text if s.__contains__("Posts")]  
+    str_match=' '.join(map(str,str_match))
+    assert "Posts" in str_match
 
 def test_login_2():
     session = requests.session()
@@ -34,7 +18,10 @@ def test_login_2():
     soup = BeautifulSoup(login, "html.parser")
     #print(soup.title.string)
     fault_text = soup.find_all(text=True)
-    assert "Wrong Login!" in fault_text
+    str_match = [s for s in fault_text if s.__contains__("Posts")]  
+    str_match=' '.join(map(str,str_match))
+    assert "Wrong Login!" in str_match
+    
 
 
 def test_login_3():
@@ -43,10 +30,11 @@ def test_login_3():
     soup = BeautifulSoup(login, "html.parser")
     #print(soup.title.string)
     fault_text = soup.find_all(text=True)
-    assert "Wrong Login!" in fault_text
+    str_match = [s for s in fault_text if s.__contains__("Posts")]  
+    str_match=' '.join(map(str,str_match))
+    assert "Wrong Login!" in str_match
 
 if __name__ == '__main__':
-    test_static()
     test_login_1()
     test_login_2()
     test_login_3()
---
+++ test/landing_page_test.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+# test that page is main page rendered properly after being deployed from war file
+
+def test_landing_page():
+    session = requests.session()
+    login = session.get("http://127.0.0.1:8080/CRMMVC/").text
+    soup = BeautifulSoup(login, "html.parser")
+    fault_text = soup.find_all(text=True)
+    str_match = [s for s in fault_text if s.__contains__("Hello")]  
+    str_match=' '.join(map(str,str_match))
+    #assert str_match == "Hello NUS!"
+    assert "Hello NUS!" in str_match
+
+
+
+
+
+if __name__ == '__main__':
+    test_landing_page()
