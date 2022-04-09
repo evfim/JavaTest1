9633e4aa1f64ee49b43014c2d4776453edb1d41b
Eric Tong <69350256+erictongg@users.noreply.github.com>
2022-04-09 22:25:14 +0800

    Delete extra/ip_addr directory

--- extra/ip_addr/ip_addr.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-#Get IP address of webapp
-hostname = socket.gethostname()
-url_name = socket.gethostbyname(hostname)
-print("The url supplied to the SAST scan is " + url_name + ".")
