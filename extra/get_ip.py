65712fc9ddcec731d49b1b2d3987948220ace1e6
Eric Tong <69350256+erictongg@users.noreply.github.com>
2022-04-09 19:21:42 +0800

    Update get_ip.py

--- extra/get_ip.py
+++ extra/get_ip.py
@@ -1,8 +1,11 @@
 import socket, os
 
+#Get IP address of webapp
 hostname = socket.gethostname()
 url_name = "http://" + socket.gethostbyname(hostname) + ":8080/CRMMVC/"
-os.system("echo" + url_name)
-url_name = "docker run -t owasp/zap2docker-stable zap-baseline.py -t " + url_name
-os.system("echo" + url_name)
-os.system(url_name)
+print("The url supplied to the SAST scan is " + url_name + ".")
+
+#Execute OWASP ZAP (SAST)
+sast_scan = "docker run -t owasp/zap2docker-stable zap-baseline.py -t " + url_name
+print("The command that ran was " + sast_scan + ".")
+os.system(sast_scan)
