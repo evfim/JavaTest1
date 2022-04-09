67a89580d09cc0693a6013a2163600f9020e3512
Eric Tong <69350256+erictongg@users.noreply.github.com>
2022-04-09 19:14:51 +0800

    Update get_ip.py

--- extra/get_ip.py
+++ extra/get_ip.py
@@ -2,7 +2,7 @@ import socket, os
 
 hostname = socket.gethostname()
 url_name = "http://" + socket.gethostbyname(hostname) + ":8080/CRMMVC/"
-print(url_name)
+os.system("echo" + url_name)
 url_name = "docker run -t owasp/zap2docker-stable zap-baseline.py -t " + url_name
-
+os.system("echo" + url_name)
 os.system(url_name)
