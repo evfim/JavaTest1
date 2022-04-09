17c94b13ccd60dd1ed07805e81c3e7b6aed291e5
Faiz Rashid <faiz.rashid@gmx.us>
2022-04-09 15:34:56 +0800

    just testing landing page.py

--- test/functional_test_login.py
+++ test/functional_test_login.py
@@ -33,29 +33,29 @@ def test_login_1():
     #print(str_match)
     #assert "Posts" in r.text
 
-def test_login_2():
-    session = requests.session()
-    login = session.post("http://127.0.0.1/CRMMVC/login", {"username": "admin", "password": "1234"}).text
-    soup = BeautifulSoup(login, "html.parser")
-    #print(soup.title.string)
-    fault_text = soup.find_all(text=True)
-    str_match = [s for s in fault_text if s.__contains__("Posts")]  
-    str_match=' '.join(map(str,str_match))
-    assert "Wrong Login!" in str_match
+#def test_login_2():
+#    session = requests.session()
+#    login = session.post("http://127.0.0.1/CRMMVC/login", {"username": "admin", "password": "1234"}).text
+#    soup = BeautifulSoup(login, "html.parser")
+#    #print(soup.title.string)
+#    fault_text = soup.find_all(text=True)
+#    str_match = [s for s in fault_text if s.__contains__("Posts")]  
+#    str_match=' '.join(map(str,str_match))
+#    assert "Wrong Login!" in str_match
     
 
 
-def test_login_3():
-    session = requests.session()
-    login = session.post("http://127.0.0.1:8080/CRMMVC/login", {"username": "admin", "password": "1234"}).text
-    soup = BeautifulSoup(login, "html.parser")
-    #print(soup.title.string)
-    fault_text = soup.find_all(text=True)
-    str_match = [s for s in fault_text if s.__contains__("Posts")]  
-    str_match=' '.join(map(str,str_match))
-    assert "Wrong Login!" in str_match
+#def test_login_3():
+#    session = requests.session()
+#    login = session.post("http://127.0.0.1:8080/CRMMVC/login", {"username": "admin", "password": "1234"}).text
+#    soup = BeautifulSoup(login, "html.parser")
+#    #print(soup.title.string)
+#    fault_text = soup.find_all(text=True)
+#    str_match = [s for s in fault_text if s.__contains__("Posts")]  
+#    str_match=' '.join(map(str,str_match))
+#    assert "Wrong Login!" in str_match
 
 if __name__ == '__main__':
     test_login_1()
-    test_login_2()
-    test_login_3()
+    #test_login_2()
+    #test_login_3()
