2f91cfd42b9f81a69b19e7334cdaa39ecbe1b8e4
Faiz Rashid <faiz.rashid@gmx.us>
2022-04-10 12:51:38 +0800

    functional test login

--- test/functional_test_login.py
+++ test/functional_test_login.py
@@ -16,21 +16,8 @@ def test_login_1():
     with requests.Session() as session:
          post = session.post(POST_LOGIN_URL, data=payload)
          soup = BeautifulSoup(post.text, "html.parser")
-         #print("soup:")
-         #print(soup)
-         #r = session.get(REQUEST_URL)
-         #print("r:")
-         #print(r.text)
 
-    #session = requests.session()
-    #login = session.post("http://127.0.0.1:8080/CRMMVC/login", {"username": "admin", "password": "1234"}).text
-    #soup = BeautifulSoup(login, "html.parser")
-    #print(soup.title.string)
     fault_text = soup.find_all(text=True)
-    print(fault_text)
-    #str_match = [s for s in fault_text if s.__contains__("Posts")]  
-    #str_match=' '.join(map(str,str_match))
-    #print(str_match)
     assert "Posts" in fault_text
 
 def test_login_2():
