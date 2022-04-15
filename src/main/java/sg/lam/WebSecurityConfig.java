47dda0eee73666af37db57cd51c62520e6d997b1
Faiz Rashid <faiz.rashid@gmx.us>
2022-04-15 23:18:16 +0800

    websecurityconfig.java

--- src/main/java/sg/lam/WebSecurityConfig.java
+++ src/main/java/sg/lam/WebSecurityConfig.java
@@ -8,7 +8,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+
 
     }
 }
