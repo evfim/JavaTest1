b0056a59bd94a733b22688c10fa84232dfc0b767
unknown <yongxian@lam.sg>
2022-04-17 02:35:17 +0800

    Add WebSecurityConfig

---
+++ src/main/java/sg/lam/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package sg.lam;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.header.HeaderWriter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
+@EnableWebMvc
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers(headers -> headers
+                .contentSecurityPolicy(csp -> csp
+                        .policyDirectives("default-src 'self'")
+                )
+        );
+    }
+}
