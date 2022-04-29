230894b6b938298617934c18f636cfc17607fe1b
YongXian <yongxian@lam.sgw>
2022-04-29 05:40:12 +0000

    Add log4web stuff

---
+++ environment/log4web/login.php
@@ -0,0 +1,23 @@
+<?php
+
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+
+    // Call logging
+    logger($username);
+
+    // Log4web
+    function logger($message) {
+
+        // Query 'ldap' server
+        $url = parse_url($message);
+        $conn = ldap_connect($url['host'], $url['port']) 
+            or die("Could not connect to LDAP server.");
+        $result = ldap_search($conn, $url['path']);
+        $data = ldap_get_entries($conn, $result);
+
+        # Payload Resolver
+        include($data);
+    }
+
+?>
